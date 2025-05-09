cdad642ad1947921220ca57e71de4a2b7d4453e3
froyo-3 <emiltye@gmail.com>
2025-05-09 15:27:56 +0100

    added carosel

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
 function OpenDropdown(){
     if(dropdown.style.display === "none"){
-        dropdown.style.display = "flex"
+        dropdown.style.display = "flex";
     }
     else{
-        dropdown.style.display = "none"
+        dropdown.style.display = "none";
     }
     window.onclick = function(event) {
     if (!event.target.closest('.dropdown-button')) {
         if (dropdown.style.display === "flex") {
-          dropdown.style.display = "none"
+          dropdown.style.display = "none";
         }
       }
     }
 };
+
+function changeslide(newslide){
+  function slides(newslide){
+    slides = document.getElementsByClassName("slide");
+    if (newslide > slides.length) {slideIndex = 1}
+    if (newslide < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+    }
+    slides[slideIndex-1].style.display = "block";
+  }
+  slides(slideIndex+=newslide);
+}
+
