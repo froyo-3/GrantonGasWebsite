45eacc2da4a24b5a594163b79653c946272576b9
froyo-3 <emiltye@gmail.com>
2025-05-18 11:59:34 +0100

    contact

---
+++ mail.php
@@ -0,0 +1,13 @@
+<?php
+	$to = "ec1829613@edinburghcollege.ac.uk";//change email address - should be client but use a student email for testing
+	$subject = "Enquiry";
+	$name = $_POST['name'];
+	$email_field = $_POST['email'];
+	$message = $_POST['message'];
+		
+	$body = "From: $name E-Mail: $email_field\n Message:\n $message\n";
+
+	echo("Thank you your message has been sent $to");
+	mail($to, $subject, $body);
+
+?>
